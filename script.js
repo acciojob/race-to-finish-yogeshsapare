4bbb9abd7666d2c37f0661a17f6aefda43e0161a
yogeshsapare <117711304+yogeshsapare@users.noreply.github.com>
2023-04-05 22:56:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+for (let i = 1; i <= 5; i++) {
+  const randomTime = Math.floor(Math.random() * 5) + 1;
+  const promise = new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`Promise ${i} resolved in ${randomTime} seconds`);
+    }, randomTime * 1000);
+  });
+  promises.push(promise);
+}
+
+Promise.any(promises)
+  .then(result => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerText = result;
+  })
+  .catch(error => console.log(error));
